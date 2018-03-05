6ddc093c602e02d00a6fa59abef31adfbfd8f8b5
Ben Rosell <benr@packtpub.com>
2018-03-05 12:49:23 +0000

    initial commit

---
+++ Magic The Gathering #1 Creatures/solution.js
@@ -0,0 +1,10 @@
+const battle = (player1, player2) => {
+  while(player1.length != player2.length) {
+    player1.length < player2.length ? player1.push([0,0]) : player2.push([0,0]);
+  }
+
+  return {
+  player1: player1.filter((item, ind) => item[1] > player2[ind][0]),
+  player2: player2.filter((item, ind) => item[1] > player1[ind][0]),
+  }
+}
